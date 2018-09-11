1c059bcbc8c4fc1f3f08128f638b6435f983c816
Stephen Kang <kang.stephen94@gmail.com>
2018-09-10 23:09:10 -0700

    Customize layout

---
+++ todolist/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () => {
+  return <div className='app-container'>
+      <div>
+        hello
+      </div>
+  </div>;
+}
+
+export default App
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+// import "normalize.css";
+// import './styles/index.css'
+
+
+ReactDOM.render(<App />,
+  document.querySelector('#root')
+);
